6c2d2977a566c0a17dc4a362f354f1b886549198
manorom <manorom@users.noreply.github.com>
2019-11-17 16:18:16 +0100

    Adds command for paypal link

--- maxtherebot/bot.py
+++ maxtherebot/bot.py
@@ -1,8 +1,14 @@
 from datetime import timedelta, datetime
 from telegram.ext import CommandHandler
+from telegram import ParseMode
 
 from .maxthere import max_there_state
 
+
+def paypallink_callback(update, context):
+    update.message.reply_text('Max denkt [das hier ist der link zum Paypal MoneyPool](https://www.paypal.com/pools/c/80WwEOPFZl)',
+                              quote=True, disable_web_page_preview=True,
+                              parse_mode=ParseMode.MARKDOWN)
 
 def maxthere_callback(update, context):
     last_seen = max_there_state.last_seen()
@@ -21,4 +27,4 @@ def help_callback(update, context):
 def configure_dispatcher(dispatcher):
     dispatcher.add_handler(CommandHandler('help', help_callback))
     dispatcher.add_handler(CommandHandler('max', maxthere_callback))
-
+    dispatcher.add_handler(CommandHandler('paypal', paypallink_callback))
